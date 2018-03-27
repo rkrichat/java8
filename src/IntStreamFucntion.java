c36352d1e8b812f01fdc73fb0bc1d3e956f34cd5
rkritchat <rojanaphruk.r@gmail.com>
2018-03-27 16:43:09 +0700

    add comment value

--- src/IntStreamFucntion.java
+++ src/IntStreamFucntion.java
@@ -10,7 +10,7 @@ public class IntStreamFucntion {
         //in case start < end system will return empty
         // ** boxed required because we need to convert int to IntStream
         List<Integer> data = IntStream.rangeClosed(1, 5).boxed().collect(Collectors.toList());
-        data.forEach(System.out::print);
+        data.forEach(System.out::print); //output: 1,2,3,4,5
 
         System.out.println();
 
@@ -18,18 +18,19 @@ public class IntStreamFucntion {
         //in case start < end system will return empty
         //** boxed required because we need to convert int io IntStream
         List<Integer> data2 = IntStream.range(6,10).boxed().collect(Collectors.toList());
-        data2.forEach(System.out::print);
+        data2.forEach(System.out::print); //output: 5,6,7,8,9,10
 
         System.out.println();
 
         //concat use for concat IntStream
         //IntStream.of() use for create IntStream and assign value to that stream
         List<Integer> data3 = IntStream.concat(IntStream.of(1,2,3), IntStream.of(4,5,6)).boxed().collect(Collectors.toList());
-        data3.forEach(System.out::print);
+        data3.forEach(System.out::print); //output: 1,2,3,4,5,6
 
         System.out.println();
 
         //generate use for generate int,if no has limit system will return empty
+        //output: random integer from 0 to 4 and return five value
         IntStream.generate(() -> new Random().nextInt(5)).limit(5).boxed().collect(Collectors.toList()).forEach(System.out::print);
     }
 }
